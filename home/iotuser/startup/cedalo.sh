8291722455e7c73688f389d25cdf552dad615667
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2023-02-19 21:50:33 +0100

    [action] fix path

--- home/iotuser/startup/cedalo.sh
+++ home/iotuser/startup/cedalo.sh
@@ -5,15 +5,15 @@ if [ "$1" == "start" ]
 then
   # Add CERTS
   echo "Run ${1} network"
-  sudo make -C /home/iotuser/projects/ -w "$1"-network
+  sudo make -C "$CURRENT_DIR"/../projects/ -w "$1"-network
   echo "Run ${1} mqtt"
-  sudo make -C /home/iotuser/projects/ -w "$1"-mqtt
+  sudo make -C "$CURRENT_DIR"/../projects/ -w "$1"-mqtt
 elif [ "$1" == "stop" ]
 then
   echo "Run ${1} mqtt"
-  sudo make -C /home/iotuser/projects/ -w "$1"-mqtt
+  sudo make -C "$CURRENT_DIR"/../projects/ -w "$1"-mqtt
   echo "Run ${1} network"
-  sudo make -C /home/iotuser/projects/ -w "$1"-network
+  sudo make -C "$CURRENT_DIR"/../projects/ -w "$1"-network
 elif [ "$1" == "build" ]
 then
   echo "Run ${1} mqtt"
@@ -21,6 +21,6 @@ then
 elif [ "$1" == "install" ]
 then
   echo "Run ${1} mqtt"
-  sudo make -C /home/iotuser/projects/ -w "$1"-mqtt
+  sudo make -C "$CURRENT_DIR"/../projects/ -w "$1"-mqtt
 fi
 
