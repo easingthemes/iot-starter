b9747470eec72e143c8573b3e1bd1cc63bc6358f
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2023-02-21 14:11:29 +0100

    [mc] log permissions

--- home/iotuser/projects/cedalo_platform/management-center/build.sh
+++ home/iotuser/projects/cedalo_platform/management-center/build.sh
@@ -21,9 +21,9 @@ if [ "skipnpm" != "${1}" ]; then
   yarn install --network-timeout 100000
   whoami
   id
-  ls -l "$SCRIPT_DIR"/src/frontend/node_modules/.cache
-  chown -R 1001:1001 "$SCRIPT_DIR"/src/frontend/node_modules/.cache && echo "pwn dat cache"
-  ls -l "$SCRIPT_DIR"/src/frontend/node_modules/.cache
+  ls -l "$SCRIPT_DIR"/src/frontend/node_modules
+  sudo chown -R $(whoami) "$SCRIPT_DIR"/src/frontend/node_modules
+  ls -l "$SCRIPT_DIR"/src/frontend/node_modules
 else
   cd ./src || exit
 fi
