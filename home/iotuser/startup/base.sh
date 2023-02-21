b256e45e24b9dc4925ec0f353df4ac40805638e1
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2023-02-21 13:12:58 +0100

    [mc] install mosquitto in the workflow

--- home/iotuser/startup/base.sh
+++ home/iotuser/startup/base.sh
@@ -4,13 +4,15 @@ node -v
 npm -v
 echo "Install global Yarn"
 npm install yarn -g
-# Create .env file
+echo "Install mosquitto"
+sudo apt install -y mosquitto mosquitto-clients
+echo "Set env vars from template"
 SED=$(which sed)
 CURRENT_DIR=$(dirname "$0")
 
 ENV_DIR=$CURRENT_DIR/../projects/cedalo_platform
 ENV_FILE="${ENV_DIR}/.env"
-echo "Set env vars from template"
+
 sudo cp "${ENV_DIR}/env.example" "$ENV_FILE"
 sudo "$SED" -i "s/mqttadminuser/$BU_USER/g" "$ENV_FILE"
 sudo "$SED" -i "s/mqttadminpass/$BU_PASS/g" "$ENV_FILE"
