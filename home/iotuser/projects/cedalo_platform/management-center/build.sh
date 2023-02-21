0378362c7710213b694134b7a42489c566c9c54b
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2023-02-21 14:27:21 +0100

    [mc] chmode cache dir

--- home/iotuser/projects/cedalo_platform/management-center/build.sh
+++ home/iotuser/projects/cedalo_platform/management-center/build.sh
@@ -20,9 +20,7 @@ if [ "skipnpm" != "${1}" ]; then
   echo "Yarn install"
   yarn install --network-timeout 100000
   mkdir -p "$SCRIPT_DIR"/src/frontend/node_modules/.cache
-  whoami
-  id
-  ls -l "$SCRIPT_DIR"/src/frontend/node_modules/.cache
+  sudo chmod -R 777 "$SCRIPT_DIR"/src/frontend/node_modules/.cache
 else
   cd ./src || exit
 fi
