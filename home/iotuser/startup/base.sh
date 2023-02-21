8ae7e85dc38565668ef2dabda5fe08975c5efed5
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2023-02-21 13:04:26 +0100

    [mc] replace multiline scripts with one line step

--- home/iotuser/startup/base.sh
+++ home/iotuser/startup/base.sh
@@ -2,6 +2,7 @@
 echo "Node: $(which node)"
 node -v
 npm -v
+echo "Install global Yarn"
 npm install yarn -g
 # Create .env file
 SED=$(which sed)
@@ -9,7 +10,7 @@ CURRENT_DIR=$(dirname "$0")
 
 ENV_DIR=$CURRENT_DIR/../projects/cedalo_platform
 ENV_FILE="${ENV_DIR}/.env"
-
+echo "Set env vars from template"
 sudo cp "${ENV_DIR}/env.example" "$ENV_FILE"
 sudo "$SED" -i "s/mqttadminuser/$BU_USER/g" "$ENV_FILE"
 sudo "$SED" -i "s/mqttadminpass/$BU_PASS/g" "$ENV_FILE"
